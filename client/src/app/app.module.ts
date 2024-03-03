d5b1fa85486e442ab46cbde58b6b88c86f61c8cb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-03 12:15:29 +0200

    Create Banner Section

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxBootstrapIconsModule, allIcons } from "ngx-bootstrap-icons";
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
